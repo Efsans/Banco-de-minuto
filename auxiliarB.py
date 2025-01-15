d9a9fe270c663a69be81edfee8993e99db1713f2
Efsans <eduardosantfreixo@gmail.com>
2025-01-14 21:39:36 -0300

    banco semi funcional

---
+++ auxiliarB.py
@@ -0,0 +1,22 @@
+import os
+while True:
+  chack = input("escreva um numero de codigo:  ")
+  if chack.isdigit() and len(chack) == 6:
+    break
+  else:
+    print("digite até seis caracteries")
+
+def comparar(chack, arquivo="contas"):
+    if os.path.exists(arquivo):
+        with open(arquivo, "r") as l:
+            for lido in l:
+                codigo = lido[:6]
+                if codigo == chack:
+                    return lido.strip()
+            print(f"o valor {chack} nao existe")
+    else:
+        print("arquivo não existe")
+
+linha = comparar(chack, arquivo="contas")
+if linha:
+    print(f"Linha encontrada: {linha}")
